28e07552220eaad378936ad01738b675d2c6a426
NIRVANA-CHEL <name@gmail.com>
2022-11-27 22:29:27 +0300

    add decorators correct mistakes

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-open_weather_token = "" # токен сервиса openweather
-tg_bot_token = "" # токен телеграм-бота
+open_weather_token = "3531753317914e591e8e0153afaa8178" # токен сервиса openweather
+tg_bot_token = "5639998213:AAECW86R1va-SLvrYA-ecIOBltPwQzYoT88" # токен телеграм-бота
--- main.py
+++ main.py
@@ -12,13 +12,19 @@ from aiogram.utils import executor
 bot = Bot(token=tg_bot_token)
 dp = Dispatcher(bot)
 
-# создаю функцию запроса погоды
-def get_weather(city, open_weather_token):
+# создаю ф-цию запроса города у пользователя
+@dp.message_handler(commands=["start"])
+async def start_command(message: types.Message):
+    await message.reply("Привет! Напиши мне название города и я пришлю сводку погоды!")
 
-    # добавляю юникоды эмоджи для визуализации наиболее частых вариантов прогноза
+
+# создаю функцию запроса погоды
+# оборачиваю ее в декоратор
+@dp.message_handler()
+async def get_weather(message: types.Message):
     code_to_smile = {
         "Clear": "Ясно \U00002600",
-        "Couds": "Облачно \U00002601",
+        "Clouds": "Облачно \U00002601",
         "Rain": "Дождь \U00002614",
         "Drizzle": "Дождь \U00002614",
         "Thunderstorm": "Гроза \U000026A1",
@@ -26,11 +32,12 @@ def get_weather(city, open_weather_token):
         "Mist": "Туман \U0001F32B"
     }
 
+    # создаю блок Try-Except для запроса и обработки информации в сервисе openweather
     try:
-        # в документации сервиса openweather получаем ссылку, по которой будем получать прогноз
+        # в документации сервиса openweather получаю ссылку, по которой будем получать прогноз
         r = requests.get(
-            f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric'
-        )
+            f'https://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={open_weather_token}&units=metric'
+         )
         data = r.json() # переменная с результатом прогноза в формате json
 
         city = data['name']
@@ -44,6 +51,7 @@ def get_weather(city, open_weather_token):
         else:
             wd = "Посмотри в окно, не пойму, что там за погода!"
 
+        # добавляю параметры прогноза
         humidity = data['main']['humidity']
         pressure = data['main']['pressure']
         wind = data['wind']['speed']
@@ -52,7 +60,8 @@ def get_weather(city, open_weather_token):
         length_of_the_day = datetime.datetime.fromtimestamp(data['sys']['sunset']) - datetime.datetime.fromtimestamp(
             data['sys']['sunrise'])
 
-        print(f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
+        # прописываю форму вывода сообщения пользователю
+        await message.reply(f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
               f'Погода в городе: {city}\nТемпература: {cur_weather}°C {wd}\n'
               f'Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/с\n'
               f'Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {length_of_the_day}\n'
@@ -60,14 +69,11 @@ def get_weather(city, open_weather_token):
               )
 
 
-    except Exception as ex: # создаю исключение на случай опечаток и ошибок в запросе города
-        print(ex)
-        print("Проверьте название города")
+    # создаю исключение на случай опечаток и ошибок в запросе города
+    except:
+        await message.reply("\U00002620 Проверьте название города \U00002620")
 
-# создаем ф-цию для запроса города у пользователя
-def main():
-    city = input("Введите название города: ")
-    get_weather(city, open_weather_token)
 
 if __name__ == '__main__':
-    main()
+    executor.start_polling(dp)
+
